c54f9a2e60014b3df0f6b1016e6543b534164dc9
Aakash Kc <aakashkcx@gmail.com>
2024-12-11 03:52:46 +0000

    update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,28 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const usersTable = sqliteTable("users_table", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
-  age: int().notNull(),
+// https://orm.drizzle.team/docs/column-types/sqlite
+
+const created = integer({ mode: "timestamp" }).default(
+  sql`(CURRENT_TIMESTAMP)`
+);
+
+const updated = integer({ mode: "timestamp" })
+  .default(sql`(CURRENT_TIMESTAMP)`)
+  .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`);
+
+export const usersTable = sqliteTable("users", {
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  username: text().notNull(),
   email: text().notNull().unique(),
+  created: created,
+  updated: updated,
+});
+
+export const tweetsTable = sqliteTable("tweets", {
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  user: text().notNull(),
+  body: text().notNull(),
+  created: created,
+  updated: updated,
 });
