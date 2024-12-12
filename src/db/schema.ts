e50e0e8d88815b27f20c17b2e3dbccef8b276b3d
Aakash Kc <aakashkcx@gmail.com>
2024-12-12 04:55:05 +0000

    add tweets + update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,28 +1,63 @@
 import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {
+  integer,
+  primaryKey,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
 
 // https://orm.drizzle.team/docs/column-types/sqlite
 
-const created = integer({ mode: "timestamp" }).default(
-  sql`(CURRENT_TIMESTAMP)`
-);
+const created = integer({ mode: "timestamp_ms" })
+  .notNull()
+  .default(sql`(CURRENT_TIMESTAMP)`);
 
-const updated = integer({ mode: "timestamp" })
+const updated = integer({ mode: "timestamp_ms" })
+  .notNull()
   .default(sql`(CURRENT_TIMESTAMP)`)
-  .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`);
+  .$onUpdate(() => new Date());
 
 export const usersTable = sqliteTable("users", {
   id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
-  username: text().notNull(),
+  username: text().notNull().unique(),
   email: text().notNull().unique(),
-  created: created,
-  updated: updated,
+  created,
+  updated,
 });
 
 export const tweetsTable = sqliteTable("tweets", {
   id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
-  user: text().notNull(),
+  user: integer()
+    .notNull()
+    .references(() => usersTable.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
   body: text().notNull(),
-  created: created,
-  updated: updated,
+  created,
+  updated,
 });
+
+export const likesTable = sqliteTable(
+  "likes",
+  {
+    user: integer()
+      .notNull()
+      .references(() => usersTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    tweet: integer()
+      .notNull()
+      .references(() => tweetsTable.id, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      }),
+    created,
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.user, table.tweet] }),
+    };
+  }
+);
