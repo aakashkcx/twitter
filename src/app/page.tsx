6ed00bc32b53a5502de880aa3796cc1e203dc482
Aakash Kc <aakashkcx@gmail.com>
2024-12-19 05:36:34 +0000

    update font weight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="flex flex-col gap-10 text-center">
-        <h1 className="text-5xl">Twitter</h1>
+        <h1 className="text-5xl font-light">Twitter</h1>
         <div className="flex gap-3 justify-center">
           <Button asChild>
             <Link href="/login">Login</Link>
