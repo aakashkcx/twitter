ccbf9e12bb7e1218c3bc81e5e8c473866b91d882
Aakash Kc <aakashkcx@gmail.com>
2024-12-19 18:24:40 +0000

    add back button

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,8 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export default function AuthLayout({
   children,
 }: Readonly<{
@@ -5,6 +10,13 @@ export default function AuthLayout({
 }>) {
   return (
     <main className="flex justify-center items-center h-screen">
+      <nav className="fixed top-3 left-3">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/">
+            <ChevronLeft />
+          </Link>
+        </Button>
+      </nav>
       {children}
     </main>
   );
