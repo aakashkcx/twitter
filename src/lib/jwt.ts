df222eca09df3bd2ad103d072d7e47bc8e78c334
Aakash Kc <aakashkcx@gmail.com>
2024-12-19 17:35:36 +0000

    add jwt session management

---
+++ src/lib/jwt.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { JWTPayload, jwtVerify, SignJWT } from "jose";
+
+const DURATION = process.env.SESSION_DURATION!;
+const JWT_ALGORITHM = process.env.SESSION_JWT_ALGORITHM!;
+const JWT_SECRET = process.env.SESSION_JWT_SECRET!;
+
+const encodedKey = new TextEncoder().encode(JWT_SECRET);
+
+export async function encrypt(payload: JWTPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: JWT_ALGORITHM })
+    .setIssuedAt()
+    .setExpirationTime(DURATION)
+    .sign(encodedKey);
+}
+
+export async function decrypt(jwt: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(jwt, encodedKey, {
+      algorithms: [JWT_ALGORITHM],
+    });
+    return payload;
+  } catch {
+    return;
+  }
+}
---
+++ src/lib/session.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+import { decrypt, encrypt } from "./jwt";
+
+const COOKIE_NAME = process.env.SESSION_COOKIE_NAME!;
+const DURATION_SECONDS = parseInt(process.env.SESSION_DURATION_SECONDS!);
+
+const cookieOptions: Partial<ResponseCookie> = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "lax",
+  path: "/",
+};
+
+export async function createSession(userId: number) {
+  const expires = new Date(Date.now() + DURATION_SECONDS * 1000);
+  const jwt = await encrypt({ userId, expires });
+  const cookieStore = await cookies();
+  cookieStore.set(COOKIE_NAME, jwt, { ...cookieOptions, expires });
+}
+
+export async function verifySession() {
+  const cookieStore = await cookies();
+  const jwt = cookieStore.get(COOKIE_NAME)?.value;
+  const session = await decrypt(jwt);
+  if (!session?.userId) return;
+  return Number(session.userId);
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete(COOKIE_NAME);
+}
