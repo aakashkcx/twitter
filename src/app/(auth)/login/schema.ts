de097c00a77cbebf4f6c0ce7608da5af190c8631
Aakash Kc <aakashkcx@gmail.com>
2024-12-20 03:46:43 +0000

    update trim

--- src/app/(auth)/login/schema.ts
+++ src/app/(auth)/login/schema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const loginSchema = z.object({
   username: z
     .string()
-    .min(3, { message: "Username must be at least 3 characters." })
-    .trim(),
+    .trim()
+    .min(3, { message: "Username must be at least 3 characters." }),
   password: z
     .string()
     .min(3, { message: "Password must be at least 3 characters." }),
--- src/app/(auth)/register/schema.ts
+++ src/app/(auth)/register/schema.ts
@@ -4,8 +4,8 @@ export const registerSchema = z
   .object({
     username: z
       .string()
-      .min(3, { message: "Username must be at least 3 characters." })
-      .trim(),
+      .trim()
+      .min(3, { message: "Username must be at least 3 characters." }),
     email: z.string().email({ message: "Invalid email address." }).trim(),
     password: z
       .string()
