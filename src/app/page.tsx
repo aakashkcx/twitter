ca77e39de8db2c60ef3de982e798e288cb5ef480
Aakash Kc <aakashkcx@gmail.com>
2024-12-29 02:50:24 +0000

    update to min height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col gap-10 text-center">
         <h1 className="text-5xl font-light">Twitter</h1>
         <div className="flex gap-3 justify-center">
