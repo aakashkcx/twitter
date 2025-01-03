d61fbae3af38cd0940c8475e762522796e2697a0
Aakash Kc <aakashkcx@gmail.com>
2025-01-03 03:00:16 +0000

    add like and unlike actions

--- src/app/(private)/actions.ts
+++ src/app/(private)/actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
 import { tweetSchema } from "@/app/(private)/schema";
-import { db, tweetsTable } from "@/db";
+import { db, likesTable, tweetsTable } from "@/db";
 import { verifySession } from "@/lib/session";
 
 export async function createTweet(
@@ -18,13 +19,53 @@ export async function createTweet(
   if (!success) return { success: false, error: "Invalid inputs." };
 
   try {
-  await db.insert(tweetsTable).values({ ...data, user: userId });
+    await db.insert(tweetsTable).values({ ...data, user: userId });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return { success: false, error: "There was a problem posting your tweet." };
   }
 
   revalidatePath(values.parent ? `/tweet/${values.parent}` : "/dashboard");
+
+  return { success: true };
+}
+
+export async function likeTweet(
+  tweetId: number,
+  path: string,
+): Promise<{ success: true } | { success: false; error: string }> {
+  const userId = await verifySession();
+  if (!userId) redirect("/login");
+
+  try {
+    await db.insert(likesTable).values({ user: userId, tweet: tweetId });
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: "There was a problem liking the tweet." };
+  }
+
+  revalidatePath(path);
+
+  return { success: true };
+}
+
+export async function unlikeTweet(
+  tweetId: number,
+  path: string,
+): Promise<{ success: true } | { success: false; error: string }> {
+  const userId = await verifySession();
+  if (!userId) redirect("/login");
+
+  try {
+    await db
+      .delete(likesTable)
+      .where(and(eq(likesTable.user, userId), eq(likesTable.tweet, tweetId)));
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: "There was a problem liking the tweet." };
+  }
+
+  revalidatePath(path);
 
   return { success: true };
 }
