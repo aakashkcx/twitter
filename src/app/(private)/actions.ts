654609e0a4466361866974439cbdbe65bd01a3a1
Aakash Kc <aakashkcx@gmail.com>
2025-01-03 02:37:56 +0000

    verifySession first, add error handling

--- src/app/(private)/actions.ts
+++ src/app/(private)/actions.ts
@@ -8,16 +8,21 @@ import { tweetSchema } from "@/app/(private)/schema";
 import { db, tweetsTable } from "@/db";
 import { verifySession } from "@/lib/session";
 
-export async function createTweet(values: z.infer<typeof tweetSchema>) {
-  const { success, data } = tweetSchema.safeParse(values);
-
-  if (!success) return { success: false, error: "Invalid inputs." };
-
+export async function createTweet(
+  values: z.infer<typeof tweetSchema>,
+): Promise<{ success: true } | { success: false; error: string }> {
   const userId = await verifySession();
-
   if (!userId) redirect("/login");
 
+  const { success, data } = tweetSchema.safeParse(values);
+  if (!success) return { success: false, error: "Invalid inputs." };
+
+  try {
   await db.insert(tweetsTable).values({ ...data, user: userId });
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: "There was a problem posting your tweet." };
+  }
 
   revalidatePath(values.parent ? `/tweet/${values.parent}` : "/dashboard");
 
